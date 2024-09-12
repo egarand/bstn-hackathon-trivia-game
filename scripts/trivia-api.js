05fa2d756c061b022d6cc61df0531e05cfb12b0f
Erica Garand <egarand13@outlook.com>
2024-09-11 22:17:07 -0400

    add optional debug mode to API class

--- scripts/trivia-api.js
+++ scripts/trivia-api.js
@@ -1,5 +1,73 @@
 class TriviaAPI {
-	constructor() {
+	constructor(debugMode = false) {
+		this.debugMode = debugMode
+
+		// TEMPORARY TESTING DATA
+		this.testQuestions = {
+			"response_code": 0,
+			"results": [
+			{
+				"type": "multiple",
+				"difficulty": "hard",
+				"category": "Geography",
+				"question": "What is the name of rocky region that spans most of eastern Canada?",
+				"correct_answer": "Canadian Shield",
+				"incorrect_answers": [
+					"Rocky Mountains",
+					"Appalachian Mountains",
+					"Himalayas"
+				]
+			},
+			{
+				"type": "multiple",
+				"difficulty": "hard",
+				"category": "Entertainment: Video Games",
+				"question": "What Pok&eacute;mon&#039;s Base Stat Total does not change when it evolves?",
+				"correct_answer": "Scyther",
+				"incorrect_answers": [
+					"Pikachu",
+					"Sneasel",
+					"Larvesta"
+				]
+			},
+			{
+				"type": "multiple",
+				"difficulty": "easy",
+				"category": "Entertainment: Video Games",
+				"question": "In Night In The Woods, where does Gregg work?",
+				"correct_answer": "Snack Falcon",
+				"incorrect_answers": [
+					"Ol&#039; Pickaxe",
+					"Video Outpost &quot;Too&quot;",
+					"Food Donkey"
+				]
+			},
+			{
+				"type": "multiple",
+				"difficulty": "medium",
+				"category": "History",
+				"question": "When Christopher Columbus sailed to America, what was the first region he arrived in?",
+				"correct_answer": "The Bahamas Archipelago",
+				"incorrect_answers": [
+					"Florida",
+					"Isthmus of Panama",
+					"Nicaragua"
+				]
+			},
+			{
+				"type": "multiple",
+				"difficulty": "easy",
+				"category": "Entertainment: Video Games",
+				"question": "What is the first weapon you acquire in Half-Life?",
+				"correct_answer": "A crowbar",
+				"incorrect_answers": [
+					"A pistol",
+					"The H.E.V suit",
+					"Your fists"
+				]
+			}
+			]
+		}
 	}
 
 	baseUrl = "https://opentdb.com/";
@@ -26,6 +94,10 @@ class TriviaAPI {
 			url.searchParams.append("category", category);
 		}
 
+		if (this.debugMode) {
+			return this.testQuestions.results;
+		}
+
 		try {
 			const response = await axios.get(url);
 			return response.data.results;
@@ -34,4 +106,10 @@ class TriviaAPI {
 			throw data;
 		}
 	}
+
+
+
+
+
+
 }
